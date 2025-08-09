e0be610b0ba5be18036c73f701d0c0e700649bca
AnggaaRestuuAlamm <anggarestualam22@gmail.com>
2025-08-09 23:25:22 +0700

    Update and rename README.md to Profile.py

---
+++ Profile.py
@@ -0,0 +1,2 @@
+# angga
+print("saya adalah seorang remaja berusia 18 tahun yang sedang belajar dan asah skill saya dalam pemrograman berbahasa python, juga seorang newbie trader")
--- README.md
+++
@@ -1,2 +0,0 @@
-# angga
-saya adalah seorang remaja berusia 18 tahun yang sedang belajar dan asah skill saya dalam pemrograman berbahasa python, juga seorang newbie trader
